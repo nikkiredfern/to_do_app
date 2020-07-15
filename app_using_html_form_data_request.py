900436b2dc6b47728631ec05b022c1caeb1b8cc0
Nicola Redfern <nikkijredfern@gmail.com>
2020-07-15 13:11:22 +1000

    mid-day commit so I can debug

--- app_using_html_form_data_request.py
+++ app_using_html_form_data_request.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,13 +17,11 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
+    description = request.form.get('description', '')
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    return redirect(url_for('index'))
 
 
 @app.route('/')
---
+++ templates/main.js
@@ -0,0 +1,26 @@
+// funstion to disable default behaviour on the todo submission form,
+// create an object, send it to the database
+document.getByElementId('form').onsubmit = function(e) {
+  e.preventDefault();
+  fetch('/todos/create', {
+    methos: 'POST',
+    body: JSON.stringify({
+      'description': document.getElementById('description').value
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  .then(function (response) {
+    return response.json();
+  });
+  .then(function (jsonResponse) {
+    console.log(jsonResponse);
+    const liItem =  dosument.createElement('LI');
+    liItem.innterHTML = jsonResponse['description'];
+    document.getElementById('todos').append(liItem);
+  })
+  .catch(function() {
+    document.getElementById('error').className ="";
+  });
+};
