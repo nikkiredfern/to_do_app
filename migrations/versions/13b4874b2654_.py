2b798dbbca2eee1432219f6b0800540091dc5b5c
Nicola Redfern <nikkijredfern@gmail.com>
2020-07-22 12:28:02 +1000

    database of app finished.

---
+++ migrations/versions/13b4874b2654_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 13b4874b2654
+Revises: 1f4910056ae8, a0b7178ab0fb
+Create Date: 2020-07-22 10:01:50.415035
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '13b4874b2654'
+down_revision = ('1f4910056ae8', 'a0b7178ab0fb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
